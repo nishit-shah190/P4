d1059d9e0aa2f9fab81f42dc657c26ec32e618b7
Akriti Saxena <143365145+asaxena8@users.noreply.github.com>
2023-09-30 16:37:30 -0400

    Add files via upload

---
+++ gettingStarted.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+import Header from './components/header/header';
+
+import Example from './components/example/Example';
+
+ReactDOM.render(
+  <div>
+  <Header/>
+  <Example />
+  </div>,
+
+  document.getElementById('reactapp'),
+);
